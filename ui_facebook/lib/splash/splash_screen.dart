c1a1db724dfdff9e9a73892652229aa565904489
hafizsiregar <hafizmessi81@gmail.com>
2021-10-03 08:27:29 +0700

    Membuat tampilan splash screen

--- ui_facebook/lib/splash/splash_screen.dart
+++ ui_facebook/lib/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key? key}) : super(key: key);
@@ -10,6 +11,55 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.only(top: 400),
+            child: Column(          
+              mainAxisAlignment: MainAxisAlignment.center, 
+              children: <Widget>[
+                Text('FACEBOOK', 
+                style: GoogleFonts.lexendExa(textStyle: TextStyle(fontSize: 18))
+                ),
+                SizedBox(height: 7,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Image.asset('assets/images/icon_fb.png',
+                      height: 18, width: 16,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Image.asset('assets/images/icon_mess.png',
+                      height: 18, width: 16,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Image.asset('assets/images/icon_ig.png',
+                      height: 18, width: 16,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Image.asset('assets/images/icon_wa.png',
+                      height: 18, width: 16,
+                      ),
+                    ),
+                    Image.asset('assets/images/icon_oracle.png',
+                    height: 18, width: 16,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
